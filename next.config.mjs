28ac5718e14bab66d612ba63f730ed652d940d70
Martin Tran <martin.tran@legato.co>
2025-02-23 13:25:58 +0700

    init source

---
+++ next.config.mjs
@@ -0,0 +1,16 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  webpack(config) {
+    config.module.rules.push({
+      test: /\.svg$/,
+      use: ["@svgr/webpack"],
+    });
+    return config;
+  },
+  images: {
+    domains: [`nextjs.org`],
+    dangerouslyAllowSVG: true,
+  },
+};
+
+export default nextConfig;
